6e0ab18d91430d8c8c4c4a56b1c9e940f761ab31
Mot <motdapchai@gmail.com>
2025-02-15 08:15:11 +0700

    Add Category Service

---
+++ Services/Service/CategoryService.cs
@@ -0,0 +1,47 @@
+﻿using BusinessObjects.Entities;
+using Repositories.IRepository;
+using Services.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Service
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepo _categoryService;
+        public CategoryService(ICategoryRepo categoryService)
+        {
+            _categoryService = categoryService;
+
+        }
+
+        public void ChangeStatus(Category category) => _categoryService.ChangeStatus(category);
+
+        public void DeleteCategory(Category category)
+        {
+            _categoryService.DeleteCategory(category);
+        }
+
+        public Category GetCategoryByID(int id)
+        {
+            return _categoryService.GetCategoryByID(id);
+        }
+
+        public IEnumerable<Category> GetCategorys() => _categoryService.GetCategorys();
+
+        public void InsertCategory(Category category)
+        {
+            _categoryService.InsertCategory(category);
+        }
+
+        public IEnumerable<Category> Search(string search) => _categoryService.Search(search);
+
+        public void UpdateCategory(Category category)
+        {
+            _categoryService.UpdateCategory(category);
+        }
+    }
+}
