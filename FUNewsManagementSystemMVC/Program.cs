0b4ba001279eb2ea94087802bb08d381221151ba
bertholdt <122198422+thethien2906@users.noreply.github.com>
2025-02-16 20:29:07 +0700

    CRUD NewsArticle

--- FUNewsManagementSystemMVC/Program.cs
+++ FUNewsManagementSystemMVC/Program.cs
@@ -11,11 +11,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<FunewsManagementContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("FUNewsManagement")));
+// Category
 builder.Services.AddScoped<ICategoryRepo, CategoryRepo>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<Repositories.IRepository.ICategoryRepo, Repositories.Repository.CategoryRepo>();
 builder.Services.AddScoped<Services.IService.ICategoryService, Services.Service.CategoryService>();
-
+// NewsArticle
+builder.Services.AddScoped<INewsArticleRepo, NewsArticleRepo>();
+builder.Services.AddScoped<INewsArticleService, NewsArticleService>();
+builder.Services.AddScoped<Repositories.IRepository.INewsArticleRepo, Repositories.Repository.NewsArticleRepo>();
+builder.Services.AddScoped<Services.IService.INewsArticleService, Services.Service.NewsArticleService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ Services/Service/NewsArticleService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects.Entities;
+using Repositories.IRepository;
+using Repositories.Repository;
+using Services.IService;
+
+
+
+namespace Services.Service
+{
+    public class NewsArticleService : INewsArticleService
+    {
+        private readonly INewsArticleRepo _newsArticleRepo;
+        public NewsArticleService(INewsArticleRepo newsArticleService)
+        {
+            _newsArticleRepo = newsArticleService;
+        }
+
+        public IEnumerable<NewsArticle> GetAllNewsArticles() => _newsArticleRepo.GetAll();
+        public NewsArticle GetNewsArticleById(string id) => _newsArticleRepo.GetNewsArticle(id);
+
+        public void CreateNewsArticle(NewsArticle newsArticle)
+        {
+            _newsArticleRepo.InsertNewsArticle(newsArticle);
+        }
+        public void UpdateNewsArticle(NewsArticle newsArticle)
+        {
+            _newsArticleRepo.UpdateNewsArticle(newsArticle);
+        }
+        public void DeleteNewsArticle(NewsArticle newsArticle)
+        {
+            _newsArticleRepo.DeleteNewsArticle(newsArticle);
+        }
+        public IEnumerable<NewsArticle> SearchNewsArticle(string search) => _newsArticleRepo.SearchNewsArticle(search);
+
+
+
+    }
+}
