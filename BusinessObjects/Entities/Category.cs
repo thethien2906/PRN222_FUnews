3ce4fe1ac3a74b57b103e6529740988da7317ad7
Mot <motdapchai@gmail.com>
2025-02-21 15:09:58 +0700

    Update for categories

--- BusinessObjects/Entities/Category.cs
+++ BusinessObjects/Entities/Category.cs
@@ -13,7 +13,7 @@ public partial class Category
 
     public short? ParentCategoryId { get; set; }
 
-    public bool? IsActive { get; set; }
+    public bool IsActive { get; set; } = true;
 
     public virtual ICollection<Category> InverseParentCategory { get; set; } = new List<Category>();
 
