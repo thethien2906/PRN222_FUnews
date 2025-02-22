9946cb4c08f0edeeb5ad22d6dfc49aa7461fda72
bertholdt <122198422+thethien2906@users.noreply.github.com>
2025-02-22 07:45:49 +0700

    Login add back to home

--- FUNewsManagementSystemMVC/obj/Debug/net8.0/FUNewsManagementSystemMVC.AssemblyInfo.cs
+++ FUNewsManagementSystemMVC/obj/Debug/net8.0/FUNewsManagementSystemMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FUNewsManagementSystemMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c12bd2ab6127dcd996962703f299d4625ffae6cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f22d3528ac39dd91d9d54552e23f8f18fe39f285")]
 [assembly: System.Reflection.AssemblyProductAttribute("FUNewsManagementSystemMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FUNewsManagementSystemMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
