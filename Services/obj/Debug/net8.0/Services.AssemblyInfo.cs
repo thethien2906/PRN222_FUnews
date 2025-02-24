419bed5d9200f64bb4fc2d7f0689983292e93152
REACT10_ThanhHoang <hoangvdtse160556@fpt.edu.vn>
2025-02-25 00:35:28 +0700

    update

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bd4192f0007f981158fb78e9452110bbee2ddfb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce067f03e82344ddebf95c9712197006b60c79c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
