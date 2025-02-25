ec8a97cb9c9297cc5f6d2409087b84c8cbd51a32
gitdev-mdat <thiminhdatdaknong@gmail.com>
2025-02-25 21:35:10 +0700

    modify UI

--- FUNewsManagementSystemMVC/obj/Release/net8.0/FUNewsManagementSystemMVC.AssemblyInfo.cs
+++ FUNewsManagementSystemMVC/obj/Release/net8.0/FUNewsManagementSystemMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FUNewsManagementSystemMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a8f0c2bd9bf6ffbda99da9e5e5a10d3e410e1dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f299ee85cfc28df26fef9c1e0332e8defe17686a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FUNewsManagementSystemMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FUNewsManagementSystemMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
