b4683799c7b8589c1ecc1d9b9f47d05a26e9bbef
REACT10_ThanhHoang <hoangvdtse160556@fpt.edu.vn>
2025-03-04 21:49:48 +0700

    update

--- DataAccessObjects/obj/Debug/net8.0/DataAccessObjects.AssemblyInfo.cs
+++ DataAccessObjects/obj/Debug/net8.0/DataAccessObjects.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessObjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e541eeabea934701704418661bf623f9b9d38202")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3d204d0cdb207f1655f3de0d5e81b488a2b4422")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessObjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessObjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e541eeabea934701704418661bf623f9b9d38202")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3d204d0cdb207f1655f3de0d5e81b488a2b4422")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
