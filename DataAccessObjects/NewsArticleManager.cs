965b7a27212a2b9bb70876e08df4c2e3520a604e
DannyVu24 <vumanhkhai2539@gmail.com>
2025-03-04 21:18:40 +0700

    Report done

--- DataAccessObjects/NewsArticleManager.cs
+++ DataAccessObjects/NewsArticleManager.cs
@@ -217,5 +217,6 @@ namespace DataAccessObjects
             }
             return articles;
         }
+
     }
 }
---
+++ FUNewsManagementSystemRazorPages/Pages/NewsArticles/Report.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using Services.DTOs;
+using Services.IService;
+
+namespace FUNewsManagementSystemRazorPages.Pages.NewsArticles
+{
+    public class ReportModel : PageModel // Đổi tên class
+    {
+        private readonly INewsArticleService _newsArticleService;
+
+        public ReportModel(INewsArticleService newsArticleService)
+        {
+            _newsArticleService = newsArticleService;
+        }
+
+        public List<NewsArticleDTO> NewsArticles { get; set; } = new List<NewsArticleDTO>();
+
+        [BindProperty(SupportsGet = true)]
+        public DateTime StartDate { get; set; } = DateTime.Now.AddMonths(-1);
+
+        [BindProperty(SupportsGet = true)]
+        public DateTime EndDate { get; set; } = DateTime.Now;
+
+        public void OnGet()
+        {
+            NewsArticles = _newsArticleService.GetNewsArticlesByPeriod(StartDate, EndDate)
+                .OrderByDescending(a => a.CreatedDate)
+                .ToList();
+        }
+    }
+}
